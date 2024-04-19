e22c9d98201303ae0ad1672f7310a3857b145067
abidi <abidinizar777@gamil.com>
2024-04-19 23:17:29 +0100

    login activity first try

--- app/src/main/java/com/example/messenger/MainActivity.java
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button txtSignUp = findViewById(R.id.sign_in);
+        txtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
