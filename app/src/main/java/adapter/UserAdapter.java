fdccba59b0fd87013dfb01396be4a6aa8df8ef4e
abidi <abidinizar777@gamil.com>
2024-05-01 15:33:53 +0100

    v2

---
+++ app/src/main/java/adapter/UserAdapter.java
@@ -0,0 +1,60 @@
+package adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.messenger.R;
+import com.example.messenger.User;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    private Context mContext;
+    private List<User> mUsers;
+
+    public UserAdapter(Context mContext, List<User> mUsers){
+        this.mUsers = mUsers;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        User user = mUsers.get(position);
+        holder.username.setText(user.getFull_name());
+        if (user.getProfile_image().equals("default")){
+            holder.profile_img.setImageResource(R.mipmap.ic_launcher);
+        } else {
+            Glide.with(mContext).load(user.getProfile_image()).into(holder.profile_img);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView username;
+        public ImageView profile_img;
+        public ViewHolder(View itemView){
+            super(itemView);
+            username = itemView.findViewById(R.id.username);
+            profile_img = itemView.findViewById(R.id.profile_img);
+        }
+    }
+}
